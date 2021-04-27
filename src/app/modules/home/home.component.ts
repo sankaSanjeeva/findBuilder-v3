b5543b50a6494bc4db70bcc8d52f413e05456f04
sanka <sanka@dspcontrols.lk>
2021-04-27 16:23:14 +0530

    Implemented home page, Updated en.json si.json files and created theme.scss file

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  categories = [
+    { id: 1, name: 'ac', link: '' },
+    { id: 2, name: 'aluminum', link: '' },
+    { id: 3, name: 'carpenter', link: '' },
+    { id: 4, name: 'cctv', link: '' },
+    { id: 5, name: 'ceiling', link: '' },
+    { id: 6, name: 'cleaner', link: '' },
+    { id: 7, name: 'contractors', link: '' },
+    { id: 8, name: 'cushionWorkers', link: '' },
+    { id: 9, name: 'doserFreight', link: '' },
+    { id: 10, name: 'electricians', link: '' },
+    { id: 11, name: 'landscaping', link: '' },
+    { id: 12, name: 'mesons', link: '' },
+    { id: 13, name: 'painters', link: '' },
+    { id: 14, name: 'plumbers', link: '' },
+    { id: 15, name: 'renters', link: '' },
+    { id: 16, name: 'solarPanel', link: '' },
+    { id: 17, name: 'stoneSandSoil', link: '' },
+    { id: 18, name: 'tilesWorkers', link: '' },
+    { id: 19, name: 'welders', link: '' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  moveTo(el: HTMLElement) {
+    el.scrollIntoView({behavior: 'smooth'});
   }
 
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { TranslocoRootModule } from '../../core/transloco/transloco-root.module';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    TranslocoRootModule
   ]
 })
 export class HomeModule { }
